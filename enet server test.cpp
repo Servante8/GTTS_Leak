22d019d20a2b32870df623616a82a3d3694984b4
Servante Gervani <75543100+Servante8@users.noreply.github.com>
2021-01-21 11:29:15 +0700

    Anak anj
    Tes

--- enet server test.cpp
+++ enet server test.cpp
@@ -1,1 +1,1 @@
-bacot, lu pasti pengen code rift cape, billboard dan lain-lain kan? dasar gratisan hadeh
+bacot, lu pasti pengen code rift cape, billboard dan lain-lain kan? Terus di rename dan dijual kan? dasar penghuni neraka hadeh
